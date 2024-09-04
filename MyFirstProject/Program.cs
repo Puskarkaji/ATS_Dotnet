f3fbc8ba33a454e261e02f537ae4c379ccd8b696
Puskar Thapa <thapapuskar53@gmail.com>
2024-09-04 08:16:01 +0545

    C# Datatypes

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// OOP WITH C#
+// C# DATATYPES
+
+// integer datatypes (ranges)
+byte personAge=5;
+short codeno=52;
+int symbolNo=2007052;
+long phoneNo=984010451;
+
+//Float datatypes (accuracy)
+float weightofElephant=8585.2255f;
+Double hugeWeight=6553536585065.56565;
+decimal valueofPi=3.14152525225525;
+
+// text datatypes
+string myname="puskar";
+char gender="m";
+
+//boolean datatypes
+bool ispresent=true;
+
+// nullable datatypes
+int? nullableint=null;
+nullableint=5;
+
+
